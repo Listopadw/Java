46cdcf76723f8b1d96640f28aff05e3b96f0af9b
Gaisin RI <113352973+Listopadw@users.noreply.github.com>
2023-02-14 10:47:30 +0300

    Add files via upload

---
+++ Java_1.js
@@ -0,0 +1,12 @@
+'Привет java!'
+var mything;
+mything=5;
+mything="Эта строка";
+var numbernine = 9;
+var stringnine ="9";
+numbernine + numbernine;
+stringnine + stringnine;
+var greeting = "Привет, ";
+var myname = "Ник";
+greeting + myname;
+myname.length;
